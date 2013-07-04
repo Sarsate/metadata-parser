794839046875b690378660ddc4beb4927163ee89
Danny Jones <dajones716@gmail.com>
2013-07-04 17:01:25 -0400

    Created parser.py and __init__.py for metadata.

---
+++ python/metadata_parser/metadata/parser.py
@@ -0,0 +1,8 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#!/usr/bin/env python
+
+""" This module contains classes and utilities for parsing the output of the mid3v2
+utility. The parser stores the information in an array of Song objects.
+"""
+
